f08686ac1662effeb2f267f9ad8b6537aeb32b43
Yashika Sridhar <yashikaedify@gmail.com>
2026-06-11 13:33:37 +0530

    Day 3 - Authentication with JWT

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
@@ -8,5 +12,7 @@ app.get("/", (req, res) => {
     status: "running"
   });
 });
+
+app.use("/api/auth", authRoutes);
 
 module.exports = app;
---
+++ src/controllers/authController.js
@@ -0,0 +1,92 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// Register
+const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists"
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email
+      }
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+};
+
+// Login
+const login = async (req, res) => {
+  try {
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid credentials"
+      });
+    }
+
+    const isMatch = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid credentials"
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d"
+      }
+    );
+
+    res.json({
+      message: "Login successful",
+      token
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+};
+
+module.exports = {
+  register,
+  login
+};
