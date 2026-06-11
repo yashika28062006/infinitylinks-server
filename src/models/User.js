34cc664b4b718766ea171184c57ff16c4a5a52bf
Yashika Sridhar <yashikaedify@gmail.com>
2026-06-11 13:27:48 +0530

    Day 2 - MongoDB integration and User model

---
+++ src/models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+
+    password: {
+      type: String
+    },
+
+    googleId: {
+      type: String
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user"
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
